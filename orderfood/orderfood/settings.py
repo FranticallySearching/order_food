dbdd6fce06a8955dc6e5f1f687e85abf36f12d6e
FranticallySearching <40616769+FranticallySearching@users.noreply.github.com>
2022-08-13 15:23:11 -0400

    FF-15 add django-dotenv

--- orderfood/orderfood/settings.py
+++ orderfood/orderfood/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
+
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# TODO FIXME Update SECRET_KEY later
-SECRET_KEY = "django-insecure-h-5_ce@jd$33hha+&tm*6^lq!!w2e=^h*e+8=w!u7l!vznse5e"
+SECRET_KEY = dotenv.read_dotenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
