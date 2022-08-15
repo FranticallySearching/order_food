57db3df01275c7bc49d1bdbfb544426e664df210
FranticallySearching <40616769+FranticallySearching@users.noreply.github.com>
2022-08-14 23:42:44 -0400

    FF-16 patch django dotenv not loading

--- orderfood/settings.py
+++ orderfood/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import utils.dotenv
+
+utils.dotenv.read_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("django_secret_key")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "false") == "true"
 
 ALLOWED_HOSTS = []
 
