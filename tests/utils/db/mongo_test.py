221ac41dbc86121832b01f0d8e8293d7c2506190
FranticallySearching <40616769+FranticallySearching@users.noreply.github.com>
2022-08-17 00:33:30 -0400

    FF-10 Fix test case

--- tests/utils/db/mongo_test.py
+++ tests/utils/db/mongo_test.py
@@ -1,25 +1,16 @@
 import uuid
 
-import pytest
-
 import utils.db.mongo
 
 
-@pytest.fixture()
-def mongo_client():
-    print("setup")
+def test_connection():
     localhost_kwargs = {
         "alias": uuid.uuid4().hex,
         "db": "mongoenginetest",
         "host": "mongomock://localhost",
     }
-    client = utils.db.mongo.connect(**localhost_kwargs)
+    mongo_client = utils.db.mongo.connect(**localhost_kwargs)
     try:
-        yield client
+        assert mongo_client.list_database_names(), "Failed to connect to Mongo! Couldn't find any database names!"
     finally:
-        client.close()
-    print("teardown")
-
-
-def test_connection(mongo_client):
-    assert mongo_client.list_database_names(), "Failed to connect to Mongo! Couldn't find any database names!"
+        mongo_client.close()
